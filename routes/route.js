f038c050d5aa58394f017049f68ff87208d048dd
stembrino <stembrino@gmail.com>
2020-02-11 20:59:19 +0000

    'updateapi'

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,19 @@
 const routes = require('express').Router();
+const request = require('request');
+const API = require('../services/api.service')
 
-
-routes.get('/', async (req, res, next)=> {
-    res.json({ success: 'server works' });
+routes.post('/location', async (req, res)=> {
+    request(API.URL_API_LOCATION, function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        const responseAPI  = JSON.parse(body)
+        console.log(responseAPI.results[0])
+        res.json(responseAPI.results[0].geometry.location);
+    })
+    
+   
 })
+
+
 
 module.exports = routes;
---
+++ services/api.service.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const API_KEY = 'AIzaSyAACsry-je5zpvzEASSqrkYY31NJWsWU3A'
+const URL_API_LOCATION = 'https://maps.googleapis.com/maps/api/geocode/json?&address=maria%20pia%20181%2C%20lisboa%2C%20lisboa&key='+API_KEY
+
+module.exports = {
+    API_KEY,
+    URL_API_LOCATION    
+}
+
+
