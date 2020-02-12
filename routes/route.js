b9a6e4628b10be0c48a0c788b6babf03e092be16
stembrino <stembrino@gmail.com>
2020-02-12 19:56:57 +0000

    'update'

--- routes/route.js
+++ routes/route.js
@@ -2,31 +2,42 @@ const routes = require('express').Router();
 const request = require('request');
 const API = require('../services/api.service')
 
-routes.get('/location', (req, res)=> {
-    
-    // let { address } = req.body
-    // console.log(req.body)
-    // request(`${API.URL_API_LOCATION}address=${ address }&key=${ API.API_KEY  }`, function (error, response, body) {
-    //     console.log('error:', error); // Print the error if one occurred
-    //     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //     const responseAPI  = JSON.parse(body)
-    //     console.log(responseAPI.results[0])
-    //     res.json(responseAPI.results[0].geometry.location);
-    // })
+routes.get('/location', (req, res) => {
+
     let location = req.query.address
-    console.log(location)
-    request(`${API.URL_API_LOCATION}address=${ location }&key=${ API.API_KEY  }`, function (error, response, body) {
+    request(`${API.URL_API_LOCATION}address=${location}&key=${API.API_KEY}`, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        const responseAPI  = JSON.parse(body)
+        const responseAPI = JSON.parse(body)
         console.log(responseAPI.results[0])
         let infoLocation = []
-        infoLocation.push({address: responseAPI.results[0].formatted_address}, {cordanates: responseAPI.results[0].geometry.location})
+        infoLocation.push({ address: responseAPI.results[0].formatted_address }, { cordanates: responseAPI.results[0].geometry.location })
 
         res.json(infoLocation);
     })
-    
-   
+
+
+})
+
+routes.get('/distance', (req, res) => {
+    let { origin, destination } = req.query
+    console.log(origin)
+    console.log(destination)
+    request(`${API.URL_API_DISTANCE}origins=${origin}&destinations=${destination}&key=${API.API_KEY}`, function (error, response, body) {
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+
+        const responseAPI = JSON.parse(body)
+        let { status } = responseAPI.rows[0].elements[0]
+        
+        if(status === "NOT_FOUND"){
+        return res.status(401).send({ error : status })
+        }
+        
+        res.json(responseAPI)
+    })
+
+
 })
 
 
--- services/api.service.js
+++ services/api.service.js
@@ -5,10 +5,12 @@
 
 const API_KEY = 'AIzaSyAACsry-je5zpvzEASSqrkYY31NJWsWU3A'
 const URL_API_LOCATION = 'https://maps.googleapis.com/maps/api/geocode/json?&'
-
+// const URL_API_DISTANCE = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&language=pt-PT&origins=&destinations=&'
+const URL_API_DISTANCE = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&language=pt-PT&'
 module.exports = {
     API_KEY,
-    URL_API_LOCATION    
+    URL_API_LOCATION,
+    URL_API_DISTANCE
 }
 
 
