d14f8a814960de6ac45ece5e8c7b330103ebcc1f
stembrino <stembrino@gmail.com>
2020-02-12 10:11:54 +0000

    update

--- services/api.service.js
+++ services/api.service.js
@@ -4,7 +4,7 @@
 
 
 const API_KEY = 'AIzaSyAACsry-je5zpvzEASSqrkYY31NJWsWU3A'
-const URL_API_LOCATION = 'https://maps.googleapis.com/maps/api/geocode/json?&address=maria%20pia%20181%2C%20lisboa%2C%20lisboa&key='+API_KEY
+const URL_API_LOCATION = 'https://maps.googleapis.com/maps/api/geocode/json?&'
 
 module.exports = {
     API_KEY,
